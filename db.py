74212447dbb4e5116389553d55940ec509a1b056
AlexMihai1804 <tapirlan.alexandru184@gmail.com>
2025-05-03 13:28:10 +0300

    add database connection and user table creation

---
+++ db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.connection = sqlite3.connect('data.db')
+        self.cursor = self.connection.cursor()
+        self.create_table()
+    def create_table(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL
+            )
+        ''')
+        self.connection.commit()
